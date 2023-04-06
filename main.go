268f34ffdcb601a0456b40c56ec1f15597a1da0e
Marc Herbrechter <mh@zebroc.de>
2023-04-06 21:26:59 +0200

    wip / Thu Apr  6 11:11:57 CEST 2023

--- main.go
+++ main.go
@@ -5,3 +5,9 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 }
+
+// AddIntToSlice adds i to s and returns it
+func AddIntToSlice(i int, s []int) []int {
+	s = append(s, i)
+	return s
+}
