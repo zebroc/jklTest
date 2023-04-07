fc589b98ed277c19316881d0541837e4f72f3354
Marc Herbrechter <mh@zebroc.de>
2023-04-07 15:43:02 +0200

    wip / Thu Apr  6 11:11:57 CEST 2023

--- main.go
+++ main.go
@@ -5,3 +5,13 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 }
+
+// Add adds a to b and returns the result
+func Add(a, b int) int {
+	return a + b
+}
+
+// Sub substracts a from b and returns the result
+func Sub(a, b int) int {
+	return a - b
+}
